42dfdebf6d3fe8bfaef4272a2f96368ad3c33993
mohamedadel6197 <mohamedadel6197@gmail.com>
2023-07-28 15:02:51 +0300

    second commit

---
+++ lib/layout/news_home_layout.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeNews extends StatelessWidget {
+  const HomeNews({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
